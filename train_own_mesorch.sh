410bf29be8bc12ea2547ee6b7cda8ee93852c003
123djy <110536420+123djy@users.noreply.github.com>
2026-02-09 15:34:47 +0800

    Add files via upload

---
+++ train_own_mesorch.sh
@@ -0,0 +1,26 @@
+
+base_dir="./训练结果/ccswin_消融_只看cswin结构_无adapter_seed46"
+mkdir -p ${base_dir}
+
+export CUDA_VISIBLE_DEVICES=0
+
+python ./train.py \
+    --model Mesorch_ConvNeXt_CSWinB \
+    --conv_pretrain True \
+    --image_size 512 \
+    --data_path "/data/jdon492/Mesorch_with_pretrain_weight/balanced_dataset.json" \
+    --test_data_path "/data/jdon492/Mesorch_with_pretrain_weight/public_datasets/IML/CASIA1_processed" \
+    --batch_size 12 \
+    --if_resizing \
+    --epochs 150 \
+    --lr 1e-4 \
+    --min_lr 5e-7 \
+    --warmup_epochs 2 \
+    --weight_decay 0.05 \
+    --accum_iter 2 \
+    --seed 46 \
+    --test_period 2 \
+    --num_workers 1 \
+    --output_dir ${base_dir}/ \
+    --log_dir ${base_dir}/ \
+2> ${base_dir}/error.log 1> ${base_dir}/logs.log
